826c1c8616c6d951007ff11a8c7fab3b3a0d4029
Alex Kavanagh <alex@ajkavanagh.co.uk>
2020-04-27 19:49:29 +0100

    Exploration on integrating Ginger with a Context in Sem r
    So this commit has a working prototype of having a deferred context
    resolver that works through the (Sem r) monad.  This was quite tricky to
    get right in terms of type signatures.  So this enables rendering with
    access to a Sem r hosted context.

---
+++ docs/03-Ginger-Context.md
@@ -0,0 +1,99 @@
+# Ginger Context issues
+
+Understanding how the Context of `GVal m` values is created is slightly
+complicated.  A 'simple' way to do it would be to attempt to produce it all up
+front, and marshall it into a single, huge, GVal.
+
+However, that's not really going to work for me.  I reckon that they will need
+to be generated on the fly, particularly (maybe) for the top level keys.
+
+I also need a way to 'register' in the top level keys with functions that
+resolve the next level of keys (or at least the value).
+
+So consider `navigation` items.  This will be things like:
+
+* pages
+* tags  - the tags and then the posts that map to those tags
+* categories - the categories and then the posts that match those categories
+* posts - all the posts (and the header data associated with it)
+* site - the site config items
+
+So something like *tags* needs to be generated from all of the headers.  But the
+*posts* could be generated as needed.  But we should generate the *tags* on
+demand.
+
+So there's a few parts to the context:
+
+1. We need a place to centralise the data.
+2. We need an effect that can gather them together and index them with top level
+   keys and convert them to `GVal m` values.
+3. We need a lookup function that will use the effect and resolve the keys.
+
+We could create out own `data` item and implement an instance of `ToGVal`. This
+would allow the lazy conversion of the key to a value and allow the value to
+generate things on demand.  Might be quite hard to do, especially in the `Sem r`
+monad!
+
+## Centralising the Data
+
+We'll have a `Context.hs` that provides a `HashMap Text (Sem r (GVal (Sem r)))`
+with some convenience functions.  The idea is that various things can register
+with the context.  To do that, we'll put it in a *Polysemy* `State`.
+
+## The Effect to gather stuff together
+
+This will effectively be a `State`, but presented with a few handy helpers, and
+a lookup function.  The lookup will return the value that can be converted to
+a `GVal m`.  We probably want to leave it as Haskell values as long as possible
+so that we can still do things with it in other contexts (e.g. counting the
+posts).
+
+## Converting the context(s) into `GVal m` values
+
+This is so that they can be consumed in Ginger.  The issue is that they probably
+need to be generated on demand.  This almost certainly means a custom data class
+and then an instance on ToGVal/GVal so that they can resolve to something at the
+appropriate time.
+
+# DSL for interacting with the Context
+
+We need to generate the mapping of top level keys (or anything else for that
+matter) to things that generate those things.  Essentially, we can adopt one of
+two principals:
+
+ 1. The Context module imports all the modules that want a 'context' item, and
+    add the function and key from it.
+ 2. The Context module provides a library and interested modules can add
+    themselves in.
+
+If we adopt the 2nd approach, then we need a 'build' phase and then a use phase,
+and they are effectively two different effects.
+
+We don't really need an effect for the building approach.  We can just have
+a function that does something like:
+
+```haskell
+registerIntoContext :: Context -> Context
+```
+
+i.e. given a context, add in a key to that `Context` object.  Then, within the
+scope of the module, it performs calls of:
+
+```haskell
+registerContextKey :: Monad m => Context -> Text -> m (GVal m) -> Context
+```
+
+i.e. take a context and a key, register in a function that returns GVal m inside
+a monad.  However, that's fairly awful, as we might want to access things on the
+context in the native format.  Obviously, that's fairly difficult in Haskell as
+you need a to have them in a variant, and that needs eventually to go to a
+`GVal m` for the rendering.
+
+So, the key is that the context brings together all of the things that might
+need to be rendered, but the individual parts (like slugs -> pages) are
+available as their own Effects.
+
+Also, individual pages will have to include the context(s) that are relevant to
+that page.  e.g. the Index will have different context keys than a context page.
+
+
---
+++ src/ExploreContext.hs
@@ -0,0 +1,309 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE ExtendedDefaultRules  #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
+
+module ExploreContext where
+
+import qualified Data.ByteString.UTF8 as DBU
+import           Data.Default         (def)
+import           Data.Function        ((&))
+import           Data.Hashable        (Hashable)
+import           Data.HashMap.Strict  (HashMap)
+import qualified Data.HashMap.Strict  as HashMap
+import           Data.Text            (Text)
+
+import           Control.Monad        (join)
+
+import           Colog.Core           (logStringStderr)
+import           Colog.Polysemy       (Log, runLogAction)
+import qualified Colog.Polysemy       as CP
+import           Polysemy             (Embed, Member, Members, Sem, embed,
+                                       embedToFinal, interpret, makeSem, run,
+                                       runFinal)
+import           Polysemy.Error       (Error)
+import qualified Polysemy.Error       as PE
+import           Polysemy.Reader      (Reader)
+import qualified Polysemy.Reader      as PR
+import           Polysemy.Writer      (Writer)
+import qualified Polysemy.Writer      as PW
+
+import           Text.Ginger          (GVal, IncludeResolver, Source,
+                                       SourceName, SourcePos, Template, ToGVal,
+                                       (~>))
+import qualified Text.Ginger          as TG
+import           Text.Ginger.Html     (Html, htmlSource)
+
+import           Effect.File          (File, FileException)
+import qualified Effect.File          as EF
+
+import           Effect.Ginger        (GingerException(..))
+
+import qualified SiteGenConfig        as S
+
+import           ExploreGinger        (parseToTemplate)
+
+{-
+   In English, I want a thing that can store bits of Text and when asked with that
+   bit of text, respond with a function that can then be run in the context of a
+   Monad m, to give as a Gval m.  I'm not even sure if this is possible!
+
+   So an @m (GVal m)@ is a thing that needs to be evalutated in the context on m.
+
+-}
+data Context m = Context { unContext :: Monad m => HashMap.HashMap Text (m (GVal m)) }
+
+{-instance (Monad m, ToGVal m v) => ToGVal m (Context m v) where-}
+    {-toGVal = TG.toGVal . unContext-}
+
+emptyContext :: Monad m => m (Context m)
+emptyContext = pure $ Context $ HashMap.empty
+
+
+-- registerIntoContext :: Context m -> Context m
+
+-- store a function that is used to resolve to a GVal m against a Text key
+registerContextKeyGVal :: Monad m => Context m -> Text -> m (GVal m) -> Context m
+registerContextKeyGVal ctxt key f = Context $ HashMap.insert key f (unContext ctxt)
+
+
+resolveContext :: Monad m => Context m -> Text -> Maybe (m (GVal m))
+resolveContext ctxt k = HashMap.lookup k $ unContext ctxt
+
+
+resolveContextM :: Monad m => Context m -> Text -> m (GVal m)
+resolveContextM ctxt k =
+    case resolveContext ctxt k of
+        Nothing -> pure def   -- a default GVal m
+        Just f' -> f'
+
+{-
+   So how do we do key1.key2 lookups?
+
+   key1 -> GVal, which Ginger will then do a lookup on that.  How do we arrange
+   things so that we still get the 'call' to resolve the 2nd lookup?  We'll have
+   to have a class that has a toGVal instance; but will that continue to run in
+   the monad?
+
+  Unfortunately, we can't: the context is supplied as a ReaderT which means that
+  it's static, and evaluated when the runReaderT is evaluated; and this is part
+  of the runGingerT:
+
+  runGingerT context tpl =
+    runReaderT (evalStateT (runExceptT (runTemplate tpl)) (defRunState tpl)) context
+
+  i.e. 'context' is the thing that we are trying to resolve to.
+
+  So at best, we can dynamically resolve the first level, but after that it needs
+  to be a static GVal.
+-}
+
+
+-- lookup the context value in the Context m hashmap.  The HashMap is in the
+-- (Sem r) monad, but this function can run in the (Run p m h) monad which can
+-- be built in to the renderer.
+contextLookupLiftRun
+    {-:: forall r b.-}
+    :: forall r.
+       {-( ToGVal (TG.Run TG.SourcePos (Sem r) Html) b-}
+       ( Member (Log String) r
+       )
+    => Context (Sem r)
+    -> Text
+    -> TG.Run TG.SourcePos (Sem r) Html (GVal (TG.Run TG.SourcePos (Sem r) Html))
+contextLookupLiftRun ctxt key = do
+    TG.liftRun $ CP.log @String $ "The key asked for was " ++ (show key)
+    case resolveContext ctxt key of
+        Nothing -> pure def
+        Just f' -> TG.liftRun (TG.marshalGVal <$> f')  -- lifts the Sem r to a TG.Run ...
+
+
+contextLookup'
+    :: forall r b.
+        ( ToGVal (Sem r) b
+        , Member (Log String) r
+        )
+    => Context (Sem r)
+    -> Text
+    -> Sem r (GVal (Sem r))
+contextLookup' ctxt key = do
+    CP.log @String $ "The key asked for was " ++ (show key)
+    resolveContextM ctxt key
+
+
+-- so we want to provide the SiteGenConfig structure as a (GVal m) under the
+-- key 'sitegen'.  This means we need to use the reader for it, and then convert
+-- it into a HashMap kind of thing with sensible keys to actually
+
+siteGenMGValM
+    :: Member (Reader S.SiteGenConfig) r
+    => Sem r (GVal (Sem r))
+siteGenMGValM = do
+    sgc <- PR.ask @(S.SiteGenConfig)
+    pure $ TG.dict
+        [ "siteYaml" ~> S.sgcSiteYaml sgc
+        , "siteID" ~> S.sgcSiteID sgc
+        , "source" ~> S.sgcSource sgc
+        , "outputDir" ~> S.sgcOutputDir sgc
+        , "extension" ~> S.sgcExtension sgc
+        , "indexPageName" ~> S.sgcIndexPageName sgc
+        , "templatesDir" ~> S.sgcTemplatesDir sgc
+        , "templateExt" ~> S.sgcTemplateExt sgc
+        , "cssDir" ~> S.sgcCssDir sgc
+        , "defaultStyle" ~> S.sgcDefaultStyle sgc
+        , "generateTags" ~> S.sgcGenerateTags sgc
+        , "generateCategories" ~> S.sgcGenerateCategories sgc
+        , "publishDrafts" ~> S.sgcPublishDrafts sgc
+        ]
+
+
+registerIntoContext
+    :: Member (Reader S.SiteGenConfig) r
+    => Context (Sem r) -> Context (Sem r)
+registerIntoContext ctxt =
+    Prelude.foldr go ctxt [ registerSiteGenIntoContext
+                          , registerBodyIntoContext
+                          , registerTitleIntoContext
+                          ]
+  where
+      go f c = f c
+
+
+registerSiteGenIntoContext
+    :: Member (Reader S.SiteGenConfig) r
+    => Context (Sem r) -> Context (Sem r)
+registerSiteGenIntoContext ctxt = registerContextKeyGVal ctxt "sitegen" siteGenMGValM
+
+
+valueInMonad :: (Monad m, ToGVal m v) => v -> m (GVal m)
+valueInMonad val = pure $ TG.toGVal val
+
+
+registerBodyIntoContext
+    :: Monad m
+    => Context m -> Context m
+registerBodyIntoContext ctxt = registerContextKeyGVal ctxt "body" (valueInMonad "The Body Text")
+
+
+registerTitleIntoContext
+    :: Monad m
+    => Context m -> Context m
+registerTitleIntoContext ctxt = registerContextKeyGVal ctxt "title" (valueInMonad "The title")
+
+
+-- and now some testing to see if it will go into a rendering system
+
+-- now let's try to construct our render function that takes the template
+renderTemplate
+    :: -- forall r b.
+       forall r.
+       -- ( ToGVal (TG.Run TG.SourcePos (Sem r) Html) b
+       {-, Member (Reader (HashMap.HashMap TG.VarName b)) r-}
+       {-( Member (Reader (Context (Sem r))) r-}
+       ( Member (Writer Text) r
+       , Member (Error GingerException) r
+       , Member (Log String) r
+       )
+    => Context (Sem r)
+    -> Template TG.SourcePos
+    -> Sem r ()
+renderTemplate ctxt tpl = do
+    {-mm <- PR.ask @(HashMap.HashMap TG.VarName b)  -- get the hashmap-}
+    {-ctxt <- PR.ask @(Context (Sem r))-}
+    let context = TG.makeContextHtmlM (contextLookupLiftRun ctxt) drainHtml
+    res <- TG.runGingerT context tpl
+    case res of
+        Left err -> PE.throw $ GingerException (show err)
+        Right _ -> pure ()
+
+
+-- get the html out (as text) -- we'll later push this to conduitT and write it
+-- into a file
+drainHtml
+    :: Member (Writer Text) r
+    => Html
+    -> Sem r ()
+drainHtml html = PW.tell $ htmlSource html
+
+
+
+-- now try to tie it all together
+
+makeTheContextM
+    :: Member (Reader S.SiteGenConfig) r
+    => Sem r (Context (Sem r))
+makeTheContextM = registerIntoContext <$> emptyContext
+
+
+-- a test function that uses the testData and the template
+renderTestContext
+    :: ( Member (Log String) r
+       , Member (Error GingerException) r
+       , Member File r
+       , Member (Error FileException) r
+       , Member (Reader S.SiteGenConfig) r
+       , Member (Writer Text) r
+       )
+    => Sem r ()
+renderTestContext = do
+    tpl <- parseToTemplate "./example-site/templates/index.html.j2"
+    ctxt <- makeTheContextM
+    renderTemplate ctxt tpl
+
+
+getSiteGenConfig
+    :: Members '[ Log String
+                , File
+                , Error FileException
+                , Error S.ConfigException ] r
+    => Sem r (S.SiteGenConfig)
+getSiteGenConfig = do
+    let fp = "./example-site/site.yaml"
+        draft = True
+    S.getSiteGenConfig fp draft
+
+
+-- Run the Sem r monad to IO
+runTestC x = x & EF.fileToIO
+               & PE.errorToIOFinal @FileException
+               & PE.errorToIOFinal @GingerException
+               & PE.errorToIOFinal @S.ConfigException
+               & PW.runWriterAssocR
+               & runLogAction @IO logStringStderr  -- [Embed IO, Error ConfigException]
+               & embedToFinal @IO
+               & runFinal
+
+runSGC x = x & EF.fileToIO
+             & PE.errorToIOFinal @FileException
+             & PE.errorToIOFinal @GingerException
+             & PE.errorToIOFinal @S.ConfigException
+             & runLogAction @IO logStringStderr  -- [Embed IO, Error ConfigException]
+             & embedToFinal @IO
+             & runFinal
+
+fromRight :: Show a => Either a b -> b
+fromRight (Right x) = x
+fromRight (Left y) = error (show y)
+
+-- something I can just call
+renderTestContextP = do
+    sgc <- (getSiteGenConfig & runSGC)
+    {-let sgc' = join $ join sgc-}
+    let sgc' = fromRight $ fromRight $ fromRight sgc
+    (PR.runReader @S.SiteGenConfig sgc' $ renderTestContext) & runTestC
